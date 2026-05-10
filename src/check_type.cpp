06ae330f3a0619685e7411e72cdd3b582d48c8d0
Isabella Basso <isabellabdoamaral@gmail.com>
2026-05-10 14:02:45 -0300

    Restore #no_copy compile-time semantics and runtime flags

--- src/check_type.cpp
+++ src/check_type.cpp
@@ -687,6 +687,7 @@ gb_internal void check_struct_type(CheckerContext *ctx, Type *struct_type, Ast *
 	struct_type->Struct.node           = node;
 	struct_type->Struct.scope          = ctx->scope;
 	struct_type->Struct.is_packed      = st->is_packed;
+	struct_type->Struct.is_no_copy     = st->is_no_copy;
 	struct_type->Struct.is_all_or_none = st->is_all_or_none;
 	struct_type->Struct.polymorphic_params = check_record_polymorphic_params(
 		ctx, st->polymorphic_params,
