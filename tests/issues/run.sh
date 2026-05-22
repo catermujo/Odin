c2e0230eda5a60f5df35328deaf18e612032ac9d
Isabella Basso <isabellabdoamaral@gmail.com>
2026-05-21 23:36:16 -0300

    checker: cover alias duplicate cases

--- tests/issues/run.sh
+++ tests/issues/run.sh
@@ -94,6 +94,12 @@ else
 	echo "SUCCESSFUL 0/1"
 	exit 1
 fi
+if [[ $($ODIN check ../test_type_switch_alias.odin $COMMON 2>&1 >/dev/null | grep -c "Error:") -eq 1 ]]; then
+	echo "SUCCESSFUL 1/1"
+else
+	echo "SUCCESSFUL 0/1"
+	exit 1
+fi
 $ODIN check ../test_issue_6979.odin -no-entry-point $COMMON_CHECK
 $ODIN test ../test_issue_7008.odin $COMMON
 $ODIN check ../test_issue_7012.odin -no-entry-point $COMMON_CHECK
