369462238e3eeac70a0342228c060f310993eb80
Isabella Basso <isabellabdoamaral@gmail.com>
2026-08-01 22:16:11 -0300

    llvm: stage procedure declarations safely

--- src/codegen_plan.hpp
+++ src/codegen_plan.hpp
@@ -1,7 +1,14 @@
+struct CodeGenProcedureImport {
+	Entity *requester;
+	Entity *procedure;
+};
+
 struct CodeGenGlobalPlan {
-	Array<Entity *> constants;
-	Array<Entity *> global_entities;
+	Array<Entity *>                 constants;
+	Array<Entity *>                 global_entities;
+	Array<CodeGenProcedureImport> procedure_imports;
 };
 
 gb_internal void codegen_build_global_plan(CodeGenGlobalPlan *plan, CheckerInfo *info, gbAllocator allocator);
 gb_internal GB_COMPARE_PROC(codegen_global_entity_cmp);
+gb_internal GB_COMPARE_PROC(codegen_procedure_import_cmp);
